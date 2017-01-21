f84b929f60335dddbfeaa200e2283673eb8fadb8
Vitor Freitas <vitor_freitas_b@hotmail.com>
2017-01-21 20:27:00 -0200

    Tests refactor

--- test/index.js
+++ test/index.js
@@ -6,6 +6,7 @@ let $div = document.createElement('div')
 document.querySelector('body').appendChild($div)
 
 describe('B', () => {
+
     it('should be a function', () => {
         expect(B).to.be.a('function')
     })
@@ -24,125 +25,175 @@ describe('B', () => {
             expect(B('body')).to.be.a.instanceof(jBit)
         })
 
-        it('get()', () => {
-            let $body = B('body').get()
-            expect($body).to.be.a('array')
-            expect($body[0]).to.equal(
+        it('should return element when accessed like an array', () => {
+            expect(B('body')[0]).to.equal(
                 document.querySelector('body')
             )
         })
 
-        it('is()', () => {
-            expect(B('body').is('body')).to.be.true
-            expect(B('.container').is('body')).to.be.false
+        describe('get()', () => {
+            let $body = B('body').get()
+
+            it('should return an array', () => {
+                expect($body).to.be.a('array')
+            })
         })
 
-        it('find()', () => {
-            let $cont = B('.container')
+        describe('is()', () => {
+            it('should return true', () => {
+                expect(B('body').is('body')).to.be.true
+            })
 
-            expect(
-                $cont.find('.container__item').get()
-            ).to.have.lengthOf(4)
-
-            expect(
-                $cont.find('.container__item--active').get()
-            ).to.have.lengthOf(1)
-
-            let $base = B('.base')
-
-            expect(
-                $base.find('li').get()
-            ).to.have.lengthOf(4)
+            it('should return false', () => {
+                expect(B('.container').is('body')).to.be.false
+            })
         })
 
-        it('filter()', () => {
-            let $items = B('.container').find('.container__item')
+        describe('find()', () => {
+            it('should find four elements', () => {
+                let $cont = B('.container')
 
-            expect(
-                $items.filter('.container__item--active').get()
-            ).to.have.lengthOf(1)
+                expect(
+                    $cont.find('.container__item').get()
+                ).to.have.lengthOf(4)
+            })
+
+            it('should find one element', () => {
+                let $cont = B('.container')
+
+                expect(
+                    $cont.find('.container__item--active').get()
+                ).to.have.lengthOf(1)
+            })
+
+            it('should find four elements', () => {
+                let $base = B('.base')
+
+                expect(
+                    $base.find('li').get()
+                ).to.have.lengthOf(4)
+            })
+
         })
 
-        it('prev()', () => {
-            expect(
-                B('.base .two').prev()[0]
-            )
-            .to.equal(document.querySelector('.base .one'))
+        describe('filter()', () => {
+            it('should find one element', () => {
+                let $items = B('.container').find('.container__item')
+
+                expect(
+                    $items.filter('.container__item--active').get()
+                ).to.have.lengthOf(1)
+            })
         })
 
-        it('next()', () => {
-            expect(
-                B('.base .one').next()[0]
-            )
-            .to.equal(document.querySelector('.base .two'))
-        })
-
-        it('prevAll()', () => {
-            let $input = B('.group').find('input[type="checkbox"]')
-
-            expect($input.prevAll().get())
-                .to.have.lengthOf(3)
-
-            expect(document.querySelector('.group span'))
-                .to.be.oneOf($input.prevAll().get())
-
-            expect($input.prevAll('span').get())
-                .to.have.lengthOf(1)
-        })
-
-        it('nextAll()', () => {
-            let $input = B('.group').find('input[type="text"]')
-
-            expect($input.nextAll().get())
-                .to.have.lengthOf(3)
-
-            expect(document.querySelector('.group span'))
-                .to.be.oneOf($input.nextAll().get())
-
-            expect($input.nextAll('span').get())
-                .to.have.lengthOf(1)
-        })
-
-        it('siblings()', () => {
-            let $span = B('.group span')
-
-            expect($span.siblings().get())
-                .to.have.lengthOf(3)
-
-            expect($span).to.not.be.oneOf(
-                $span.siblings().get()
-            )
-        })
-
-        it('children()', () => {
-            let $cont = B('.container')
-
-            expect($cont.children().get())
-                .to.have.lengthOf(4)
-
-            expect(
-                $cont.children('.container__item--active').get()
-            )
-            .to.have.lengthOf(1)
-
-            let $active = $cont.find('.container__item--active')
-
-            expect($active.children().get())
-                .to.be.empty
-        })
-
-        it('parent()', () => {
-            let $base2 = B('.base-2')
-
-            expect($base2.parent()[0])
-                .to.be.equal(
-                    document.querySelector('li.two')
+        describe('prev()', () => {
+            it('should return the preceding element', () => {
+                expect(
+                    B('.base .two').prev()[0]
                 )
+                .to.equal(document.querySelector('.base .one'))
+            })
+        })
 
-            expect($base2.parent().parent()[0])
-                .to.be.equal(
-                    document.querySelector('.base')
+        describe('next()', () => {
+            it('should return the following element', () => {
+                expect(
+                    B('.base .one').next()[0]
                 )
+                .to.equal(document.querySelector('.base .two'))
+            })
+        })
+
+        describe('prevAll()', () => {
+            it('should return all preceding elements', () => {
+                let $input = B('.group').find('input[type="checkbox"]')
+
+                expect($input.prevAll().get())
+                    .to.have.lengthOf(3)
+
+                expect(document.querySelector('.group span'))
+                    .to.be.oneOf($input.prevAll().get())
+            })
+
+            it('should return all preceding elements filtered', () => {
+                let $input = B('.group').find('input[type="checkbox"]')
+
+                expect($input.prevAll('span').get())
+                    .to.have.lengthOf(1)
+            })
+        })
+
+        describe('nextAll()', () => {
+            it('should return all following elements', () => {
+                let $input = B('.group').find('input[type="text"]')
+
+                expect($input.nextAll().get())
+                    .to.have.lengthOf(3)
+
+                expect(document.querySelector('.group span'))
+                    .to.be.oneOf($input.nextAll().get())
+            })
+
+            it('should return all following elements filtered', () => {
+                let $input = B('.group').find('input[type="text"]')
+
+                expect($input.nextAll('span').get())
+                    .to.have.lengthOf(1)
+            })
+        })
+
+        describe('siblings()', () => {
+            it('should return all siblings', () => {
+                let $span = B('.group span')
+
+                expect($span.siblings().get())
+                    .to.have.lengthOf(3)
+            })
+
+            it('should not contain the the selected element', () => {
+                let $span = B('.group span')
+
+                expect($span).to.not.be.oneOf(
+                    $span.siblings().get()
+                )
+            })
+        })
+
+        describe('children()', () => {
+            it('should return element children nodes', () => {
+                let $cont = B('.container')
+
+                expect($cont.children().get())
+                    .to.have.lengthOf(4)
+            })
+
+            it('should return element children nodes filtered', () => {
+                let $cont = B('.container')
+
+                expect(
+                    $cont.children('.container__item--active').get()
+                )
+                .to.have.lengthOf(1)
+            })
+
+            it('should return empty in case of no children', () => {
+                let $cont = B('.container')
+                  , $active = $cont.find('.container__item--active')
+
+                expect($active.children().get())
+                    .to.be.empty
+            })
+        })
+
+        describe('parent()', () => {
+            it('should return the element parent node', () => {
+                let $base2 = B('.base-2')
+
+                expect($base2.parent()[0])
+                    .to.be.equal(
+                        document.querySelector('li.two')
+                    )
+            })
         })
     })
 })
