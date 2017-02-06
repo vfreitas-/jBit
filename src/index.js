cdd0a1166a7a9c5e0e82f573b70eee839c370470
Vitor Freitas <vitor_freitas_b@hotmail.com>
2017-02-06 09:04:47 -0200

    1.1.2

--- src/index.js
+++ src/index.js
@@ -16,24 +16,4 @@ function $ (selector, context) {
     return new jBit(selector, context)
 }
 
-var
-
-	// Map over jQuery in case of overwrite
-	_jQuery = window.jQuery,
-
-	// Map over the $ in case of overwrite
-	_$ = window.$;
-
-$.noConflict = () => {
-	if ( window.$ === jQuery ) {
-		window.$ = _$;
-	}
-
-	if ( deep && window.jQuery === jQuery ) {
-		window.jQuery = _jQuery;
-	}
-
-	return jQuery;
-}
-
 export default $
